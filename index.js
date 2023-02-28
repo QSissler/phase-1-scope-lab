0af2f4766808de4606062f0eebbe6eda288bb047
Quinn Sissler <QSissler@gmail.com>
2023-02-27 19:00:31 -0500

    commit tree 36e491b82d4b8fcd748835367c8fee77c5986b2a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+   var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'dawitt'
+
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = 'someone else'
+}
